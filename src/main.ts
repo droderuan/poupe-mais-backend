47aa6fcc523ed11ec39b4dbc6822b6750a0d03ba
Ruan Ferreira <droderuan@hotmail.com>
2021-05-05 01:39:07 -0300

    fix: :bug: Fix cors

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: false });
+  const app = await NestFactory.create(AppModule);
+
+  const options = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept',
+  };
+
+  app.enableCors(options);
+
   await app.listen(3000);
 }
 bootstrap();
